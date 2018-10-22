8f4a1cfff4d9d856040a77f6ab01fadc2346f93e
git <1q2w3e4r>
2018-10-22 19:19:04 +0800

    10月优化功能

--- lawyer/case/doc/doc_master.py
+++ lawyer/case/doc/doc_master.py
@@ -1,7 +1,18 @@
 # coding=utf8
 
-from util.redis_util import RedisCasePlanSchemaTaskMaster
 
-if __name__ == "__main":
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S', filemode='a', )
+
+if __name__ == "__main__":
+    logging.info("master====begin==========")
+    from util.redis_util import RedisCasePlanSchemaTaskMaster
     while True:
-        RedisCasePlanSchemaTaskMaster.refresh_case_plan_schema(RedisCasePlanSchemaTaskMaster.KEY, 500)  # 抓取任务队列
+        try:
+            RedisCasePlanSchemaTaskMaster.refresh_case_plan_schema(RedisCasePlanSchemaTaskMaster.KEY, 500)  # 抓取任务队列
+        except Exception as e:
+            logging.exception("发生错误:->")
+        time.sleep(15)
---
+++ lawyer/case/doc/doc_master.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+nohup python -u doc_master.py > doc_master.out 2>&1 &
